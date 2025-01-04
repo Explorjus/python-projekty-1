f27cbe315a2f87ecb9229eb76953343e594cade6
Explorjus <ja.kubson022@gmail.com>
2025-01-04 23:30:18 +0100

    nowe

--- from dotenv import load_dotenv.py
+++ from dotenv import load_dotenv.py
@@ -3,7 +3,7 @@ import os
 
 load_dotenv("/Users/explorjus/Python_1/.gitignore/.env.py")
 
-# Pobieranie kluczy z zmiennych środowiskowych
+# Pobieranie kluczy z zmiennych środowiskowy
 api_key = os.getenv("API_KEY")
 secret_key = os.getenv("SECRET_KEY")
 
