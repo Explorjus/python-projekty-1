a9c1a0718d2c75d1e997af6ecd373d6a202e0780
Explorjus <ja.kubson022@gmail.com>
2025-01-05 17:31:42 +0100

    nowe

--- YT_import.py
+++ YT_import.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import subprocess
 
-link = str(input("Enter the link of the video: "))
+link = str(input("Wpisz link do filu z youtube: "))
 save_path = "/Users/explorjus/Wideo"
 
 # Upewnij się, że katalog docelowy istnieje
@@ -14,16 +14,16 @@ try:
     response = requests.get(link)
     soup = BeautifulSoup(response.content, "html.parser")
     title = soup.find("meta", property="og:title")["content"]
-    print(f"Title: {title}")
+    print(f"Tytuł: {title}")
     
-    print("Downloading in the best quality...")
+    print("Pobieranie...")
     subprocess.run([
         "yt-dlp",
         "-f", "bestvideo+bestaudio/best",  # Pobieranie najlepszej jakości wideo i audio
         "-o", os.path.join(save_path, "%(title)s.%(ext)s"),
         link
     ])
-    print("Finished downloading")
-    print("Ready to go")
+    print("Ukończono")
+    print("Gotowe")
 except Exception as e:
     print(f"An error occurred: {e}")
